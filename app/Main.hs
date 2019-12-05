58cee32d92e34dc4df109589802aa8c31ec590a8
Jordan Rome <jordalgo@fb.com>
2019-12-05 04:59:11 -0500

    day 4 part2'

--- app/Main.hs
+++ app/Main.hs
@@ -7,4 +7,4 @@ main :: IO ()
 main = do
     contents <- readFile "src/input/day4.txt"
     putStrLn (show (getPart1 (lines contents)))
-    --putStrLn (show (getPart2 (lines contents)))
+    putStrLn (show (getPart2 (lines contents)))
--- src/Day4.hs
+++ src/Day4.hs
@@ -1,5 +1,6 @@
 module Day4
     ( getPart1
+    , getPart2
     ) where
 
 import Data.List
@@ -10,10 +11,21 @@ areDigitsAsc xs = sort xs == xs
 hasSameAdjacentDigits :: String -> Bool
 hasSameAdjacentDigits = any (>1) . map length . group
 
+hasDouble :: String -> Bool
+hasDouble = any (2==) . map length . group
+
 getPart1Amount :: (Int, Int) -> Int
 getPart1Amount (start, end) = length (filter (\x -> (areDigitsAsc x) && (hasSameAdjacentDigits x)) (map show [start..end]))
 
 getPart1 :: [String] -> Int
 getPart1 input = getPart1Amount ((nums !! 0), (nums !! 1))
+  where
+  nums = map read input
+
+getPart2Amount :: (Int, Int) -> Int
+getPart2Amount (start, end) = length (filter (\x -> (areDigitsAsc x) && (hasDouble x)) (map show [start..end]))
+
+getPart2 :: [String] -> Int
+getPart2 input = getPart2Amount ((nums !! 0), (nums !! 1))
   where
   nums = map read input
