5d19e4c8a1a3c12ea9aaf0d40899c9c51fc4ca42
Jordan Rome <jordalgo@fb.com>
2019-12-05 05:06:11 -0500

    clean up day4

--- src/Day4.hs
+++ src/Day4.hs
@@ -8,24 +8,28 @@ import Data.List
 areDigitsAsc :: String -> Bool
 areDigitsAsc xs = sort xs == xs
 
+getGroupLengths :: String -> [Int]
+getGroupLengths = map length . group
+
 hasSameAdjacentDigits :: String -> Bool
-hasSameAdjacentDigits = any (>1) . map length . group
+hasSameAdjacentDigits = any (>1) . getGroupLengths
 
 hasDouble :: String -> Bool
-hasDouble = any (2==) . map length . group
+hasDouble = any (2==) . getGroupLengths
 
 getPart1Amount :: (Int, Int) -> Int
 getPart1Amount (start, end) = length (filter (\x -> (areDigitsAsc x) && (hasSameAdjacentDigits x)) (map show [start..end]))
 
-getPart1 :: [String] -> Int
-getPart1 input = getPart1Amount ((nums !! 0), (nums !! 1))
+getRange :: [String] -> (Int, Int)
+getRange input = ((nums !! 0), (nums !! 1))
   where
   nums = map read input
+
+getPart1 :: [String] -> Int
+getPart1 = getPart1Amount . getRange
 
 getPart2Amount :: (Int, Int) -> Int
 getPart2Amount (start, end) = length (filter (\x -> (areDigitsAsc x) && (hasDouble x)) (map show [start..end]))
 
 getPart2 :: [String] -> Int
-getPart2 input = getPart2Amount ((nums !! 0), (nums !! 1))
-  where
-  nums = map read input
+getPart2 = getPart2Amount . getRange
