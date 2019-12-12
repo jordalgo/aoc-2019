7b2b24acbf10e5c79da9820fcca97b410931d023
Jordan Rome <jordalgo@fb.com>
2019-12-12 09:53:50 -0500

    day6 part 2

--- src/Day6.hs
+++ src/Day6.hs
@@ -6,6 +6,7 @@ module Day6
 import Data.List
 import Data.List.Index
 import Data.List.Split
+import qualified Data.Map as Map
 
 type Edge = (String, String)
 
@@ -19,11 +20,14 @@ getEdge str = (node1, node2)
 getRootEdge :: [Edge] -> Edge
 getRootEdge = head . filter (\(head, tail) -> head == "COM")
 
+splitEdgesWithMatch :: String -> [Edge] -> ([Edge], [Edge])
+splitEdgesWithMatch match = foldl (\(m, r) edge -> if (match == (fst edge)) then (edge:m, r) else (m, edge:r)) ([], [])
+
 getIndirectCount :: [Edge] -> Int -> String -> Int
-getIndirectCount [] count left = count
+getIndirectCount [] count _ = count
 getIndirectCount edges count left = foldl (\acc (_, mR) -> acc + (getIndirectCount rest (count + 1) mR)) count matches
   where
-  (matches, rest) = foldl (\(m, r) edge -> if (left == (fst edge)) then (edge:m, r) else (m, edge:r)) ([], []) edges
+  (matches, rest) = splitEdgesWithMatch left edges
 
 -- direct orbits are the number of initial edges
 getPart1 xs = (length edges) + (getIndirectCount edges 0 (snd rootEdge))
@@ -31,5 +35,17 @@ getPart1 xs = (length edges) + (getIndirectCount edges 0 (snd rootEdge))
   edges = map getEdge xs
   rootEdge = getRootEdge edges
 
-getPart2 :: [String] -> String
-getPart2 (head:tail) = head
+getPathToCOM :: String -> Map.Map String String -> [String]
+getPathToCOM x parentMap
+  | x == "COM" = []
+  | otherwise = parent : (getPathToCOM parent parentMap)
+  where
+    parent = parentMap Map.! x
+
+getPart2 xs = (+) (length $ pathToYOU \\ pathToSAN) (length $ pathToSAN \\ pathToYOU)
+  where
+  edges = map getEdge xs
+  parentMap = Map.fromList $ map (\(a,b) -> (b, a)) edges
+  pathToYOU = getPathToCOM "YOU" parentMap
+  pathToSAN = getPathToCOM "SAN" parentMap
+
