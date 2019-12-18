8a8dafc5b76bee78995406a3963c0316b0fc6985
Jordan Rome <jordalgo@fb.com>
2019-12-18 10:31:31 -0500

    day 8 part2

--- src/Day8.hs
+++ src/Day8.hs
@@ -23,4 +23,23 @@ getPart1 str = (getCount 1 foundLayer) * (getCount 2 foundLayer)
   zeroIndex = elemIndex (minimum zeros) zeros
   foundLayer = layers !! (getMaybeInt zeroIndex)
 
-getPart2 str = 5
+getPixel :: Int -> Int -> Int
+getPixel a b
+  | a == 2 = b
+  | otherwise = a
+
+getPixelRow :: [Int] -> [Int] -> [Int]
+getPixelRow acc next = imap (\index accItem -> getPixel accItem (next !! index)) acc
+
+convertToColor:: [Int] -> [Char]
+convertToColor xs = map (\x -> if (x == 0) then '-' else 'x') xs
+
+getPart2 :: String -> [[Char]]
+getPart2 str = map (\x -> convertToColor x) picture
+  where
+  transparentLayer = replicate 6 (replicate 25 2)
+  numInputs = map (\c -> read [c]) str
+  chunks = chunksOf (25 * 6) numInputs
+  layers = map (chunksOf 25) chunks
+  picture = foldl (\acc layer -> imap (\index accRow -> getPixelRow accRow (layer !! index)) acc) transparentLayer layers
+
