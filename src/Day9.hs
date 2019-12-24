a2edde4015683557e680b33971d73a447c0c83c1
Jordan Rome <jordalgo@fb.com>
2019-12-23 21:32:20 -0500

    make State a record

--- src/Day9.hs
+++ src/Day9.hs
@@ -30,20 +30,26 @@ getWriteParamFromMode mode pos relativeBase program
   val = program !! pos
   nextRelativeBase = relativeBase + val
 
-type State = (Int, Int, [Int], Int, [Int])
+data State = State {
+  code :: Int,
+  output :: [Int],
+  position :: Int,
+  program :: [Int],
+  relativeBase :: Int
+} deriving (Eq, Ord, Show)
 
 intCodeComputer :: [Int] -> State -> State
-intCodeComputer inputs (position, relBase, xs, code, output)
-  | code == 1 = (posP4, relBase, (setAt param3 (param1 + param2) extendedProgram), code, output)
-  | code == 2 = (posP4, relBase, (setAt param3 (param1 * param2) extendedProgram), code, output)
-  | code == 3 = (posP2, relBase, (setAt param3 input extendedProgram), code, output)
-  | code == 4 = (posP2, relBase, xs, code, (output ++ [param1]))
-  | code == 5 = ((if (param1 > 0) then param2 else posP3), relBase, xs, code, output)
-  | code == 6 = ((if (param1 == 0) then param2 else posP3), relBase, xs, code, output)
-  | code == 7 = (posP4, relBase, (setAt param3 (if (param1 < param2) then 1 else 0) extendedProgram), code, output)
-  | code == 8 = (posP4, relBase, (setAt param3 (if (param1 == param2) then 1 else 0) extendedProgram), code, output)
-  | code == 9 = (posP2, (relBase + param1), xs, code, output)
-  | code == 99 = (position, relBase, xs, code, output)
+intCodeComputer inputs (State {output = output, position = position, program = xs, relativeBase = relBase})
+  | code == 1 = State {code = code, position = posP4, program = (setAt param3 (param1 + param2) extendedProgram), relativeBase = relBase, output = output}
+  | code == 2 = State {code = code, position = posP4, program = (setAt param3 (param1 * param2) extendedProgram), relativeBase = relBase, output = output}
+  | code == 3 = State {code = code, position = posP2, program = (setAt param3 input extendedProgram), relativeBase = relBase, output = output}
+  | code == 4 = State {code = code, position = posP2, program = xs, relativeBase = relBase, output = (output ++ [param1])}
+  | code == 5 = State {code = code, position = ((if (param1 > 0) then param2 else posP3)), program = xs, relativeBase = relBase, output = output}
+  | code == 6 = State {code = code, position = ((if (param1 == 0) then param2 else posP3)), program = xs, relativeBase = relBase, output = output}
+  | code == 7 = State {code = code, position = posP4, program = (setAt param3 (if (param1 < param2) then 1 else 0) extendedProgram), relativeBase = relBase, output = output}
+  | code == 8 = State {code = code, position = posP4, program = (setAt param3 (if (param1 == param2) then 1 else 0) extendedProgram), relativeBase = relBase, output = output}
+  | code == 9 = State {code = code, position = posP2, program = xs, relativeBase = (relBase + param1), output = output}
+  | code == 99 = State {code = code, position = posP2, program = xs, relativeBase = relBase, output = output}
   | otherwise = error "bad opcode"
   where
     instruction = xs !! position
@@ -66,16 +72,11 @@ getProgram = map read . splitOn ","
 
 getPart1 str = do
   let program = getProgram str
-      initialState = (0, 0, [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99], 0, [])
-      initialState2 = (0, 0, [1102,34915192,34915192,7,4,7,99,0], 0, [])
-      initialState3 = (0, 0, [104,1125899906842624,99], 0, [])
-      initialState4 = (0, 0, program, 0, [])
-      initialState5 = (0, 0, [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31, 1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104, 999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99], 0, [])
+      initialState4 = State {code = 0, position = 0, program = program, relativeBase = 0, output = []}
       runProgram state = do
         let nextState = intCodeComputer [1] state
-            (position, relBase, xs, code, output) = nextState
-        putStrLn (if (code == 4) then (show nextState) else "")
-        if (code == 99) then return() else runProgram nextState
+            c = code nextState
+        if (c == 99) then putStrLn (show (output nextState)) else runProgram nextState
   runProgram initialState4
 
 getPart2 str = 6
